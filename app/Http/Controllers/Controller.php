c546e628e3486047cb3f9ebc50ff40dc461bddc5
felixmind <felixmind@outlook.com>
2020-07-19 12:43:25 +0300

    Базовый метод форматирования JSON ответа

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Возвращает ответ в формате JSON без экранирования слэшей.
+     *
+     * @param $value
+     *
+     * @return JsonResponse
+     */
+    protected function jsonResponse($value): JsonResponse
+    {
+        return response()->json($value)
+                         ->setEncodingOptions(JSON_UNESCAPED_SLASHES);
+    }
 }
