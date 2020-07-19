ebe50d9122628c48dde09f4e37d8e51ae195db9e
felixmind <felixmind@outlook.com>
2020-07-19 12:44:21 +0300

    Прописаны маршруты ресурсов

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::apiResource('clients', 'ClientController');
+Route::apiResource('clients.phones', 'ClientPhoneController');
+Route::apiResource('clients.emails', 'ClientEmailController');
